94a5f75d4caea23f1630e08ba8c04c5a9bb27353
allanjames <james.allan@brafton.com>
2019-02-14 16:01:51 -0500

    have executed a successful check, export of a video and put of a tag to exported video

--- classes/BraftonClient.php
+++ classes/BraftonClient.php
@@ -3,8 +3,9 @@
 class BraftonClient {
 
     public $d = "api.brafton.com";
-    public $pb = "SR1Y0OC4"; //Brafton public key
-    public $pv = "57ead238-3fb9-4678-995a-f27ec23dae89"; //Brafton private key
+    public $pb = "9UREC71R"; //Brafton public key
+    public $pv = "3a66f24b-5306-46b1-ba74-bd21700e573c"; //Brafton private key
+    public $videos  = array();
 
     public function __construct(){
 
@@ -13,14 +14,11 @@ class BraftonClient {
     public function getBraftonVideos() {
 
         $domain = preg_replace('/https:\/\//','',"api.brafton.com");
-        echo '<br />'.$domain;
+        //echo '<br />'.$domain;
         $baseURL = 'http://livevideo.'.str_replace('http://', '',$domain).'/v2/';
-        echo '<br />'.$baseURL;
-        //die();
-        $videoClient = new AdferoVideoClient($baseURL, $this->pb, $this->pv);
+        //echo '<br />'.$baseURL;        
         $client = new AdferoClient($baseURL, $this->pb, $this->pv);
-        $videoOutClient = $videoClient->videoOutputs();
-
+        
         $photos = $client->ArticlePhotos();
         $photoURI = 'http://'.str_replace('api', 'pictures',$domain).'/v2/';
         $photoClient = new AdferoPhotoClient($photoURI);
@@ -33,6 +31,36 @@ class BraftonClient {
         $articles = $articleClient->ListForFeed($feedList->items[0]->id,'live',0,100);
         $articles_imported = 0;
         $articles->items = array_reverse($articles->items);
-        return $articles->items;
+        
+        foreach($articles->items as $item){
+
+            $article = $client->Articles()->Get($item->id);
+            //$this->videos[$item->id] = $article->fields['title'];
+            array_push($this->videos,array(
+                        'brafton-id'=>$item->id,
+                        'title'=>$article->fields['title']
+            ));
+        }
+        return $this->videos;
+    }
+    public function getVideoSource($brafton_id){
+        $domain = preg_replace('/https:\/\//','',"api.brafton.com");
+        $baseURL = 'http://livevideo.'.str_replace('http://', '',$domain).'/v2/';
+        $videoClient = new AdferoVideoClient($baseURL, $this->pb, $this->pv);
+        $videoOutClient = $videoClient->videoOutputs();
+        $videoList=$videoOutClient->ListForArticle($brafton_id,0,10);
+        $list=$videoList->items;
+        foreach($list as $listItem){
+            $output=$videoOutClient->Get($listItem->id);
+            $type = $output->type;
+            $path = $output->path;
+            $resolution = $output->height;
+            continue;
+        }
+        return $path;
+    }
+    public function getVideoTitle($client){
+        
+        var_dump($client);
     }
 }
--- exporter.php
+++ exporter.php
@@ -9,18 +9,30 @@ include './classes/VimeoPost.php';
 
 $brafton = new BraftonClient();
 $list = $brafton->getBraftonVideos();
+
 $vimeo = new VimeoPost('https://api.vimeo.com/users/92409741');
 $group = $vimeo->checkVideos();
 $braftonIds = array();
 foreach($group as $video){
     if(sizeOf($video->tags)>0){
-        //echo '<br />'.$video->tags[0]->name;
         array_push($braftonIds,$video->tags[0]->name);
     }    
 }
-var_dump($list[0]->id);
-foreach($list as $item){
-    if(in_array($item->id,$braftonIds)){
-        echo 'video '.$item->id.' already exists';
+
+
+foreach($list as $a){
+    if(in_array($a['brafton-id'],$braftonIds)){
+        echo 'video '.$a['brafton-id'].' already exists';
+    } else{
+        //echo '<br />'.$a['brafton-id']. $a['title'].$brafton->getVideoSource($a['brafton-id']);
+        $obj = array(
+            "upload"=> array(
+                "approach"=>"pull",
+                "link"=> $brafton->getVideoSource($a['brafton-id'])
+            ),
+            "name"=> $a['title']
+        );
+        $vimeo->postVideo(json_encode($obj),$a['brafton-id']);
+        die();
     }
 }
